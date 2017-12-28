ec5b3e5bdeb4bc1d6d7e321ed9a33d963b682e82
pmknkaek <cakesokorigins@gmail.com>
2017-12-27 22:56:50 -0700

    app is now immersive (thank goodness)

--- android/src/com/musicalpastries/superboopers/AndroidLauncher.java
+++ android/src/com/musicalpastries/superboopers/AndroidLauncher.java
@@ -1,23 +1,133 @@
 package com.musicalpastries.superboopers;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.musicalpastries.superboopers.SuperBoopers;
 
 public class AndroidLauncher extends AndroidApplication {
+	private int currentApiVersion;
+	//TODO:
+	int flagsJ /*= View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+			| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+			| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+			| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+			| View.SYSTEM_UI_FLAG_FULLSCREEN*/;
+
+	final int flagsK = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+			| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+			| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+			| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+			| View.SYSTEM_UI_FLAG_FULLSCREEN
+			| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new SuperBoopers(), config);
-		View decorView = getWindow().getDecorView();
 
-		int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-				| View.SYSTEM_UI_FLAG_FULLSCREEN;
-		decorView.setSystemUiVisibility(uiOptions);
+		currentApiVersion = android.os.Build.VERSION.SDK_INT;
+
+		if(currentApiVersion >= Build.VERSION_CODES.KITKAT)
+		{
+			getWindow().getDecorView().setSystemUiVisibility(flagsK);
+			// Code below is to handle presses of Volume up or Volume down.
+			// Without this, after pressing volume buttons, the navigation bar will
+			// show up and won't hide
+			final View decorView = getWindow().getDecorView();
+			decorView
+					.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener()
+					{
+						@Override
+						public void onSystemUiVisibilityChange(int visibility)
+						{
+							if((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0)
+							{
+								decorView.setSystemUiVisibility(flagsK);
+							}
+						}
+					});
+		}
+		else if(currentApiVersion >= Build.VERSION_CODES.JELLY_BEAN)
+		{
+			getWindow().getDecorView().setSystemUiVisibility(flagsJ);
+			// Code below is to handle presses of Volume up or Volume down.
+			// Without this, after pressing volume buttons, the navigation bar will
+			// show up and won't hide
+			final View decorView = getWindow().getDecorView();
+			decorView
+					.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener()
+					{
+						@Override
+						public void onSystemUiVisibilityChange(int visibility)
+						{
+							if((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0)
+							{
+								decorView.setSystemUiVisibility(flagsJ);
+							}
+						}
+					});
+		}
+
+		initialize(new SuperBoopers(), config);
+	}
+
+	@Override
+	public void onWindowFocusChanged(boolean hasFocus) {
+		super.onWindowFocusChanged(hasFocus);
+		if (currentApiVersion >= Build.VERSION_CODES.KITKAT && hasFocus) {
+			getWindow().getDecorView().setSystemUiVisibility(flagsK);
+		}
+		else if (currentApiVersion >= Build.VERSION_CODES.JELLY_BEAN && hasFocus) {
+			getWindow().getDecorView().setSystemUiVisibility(flagsK);
+		}
+	}
+
+	@Override
+	protected void onResume(){
+		super.onResume();
+		if(currentApiVersion >= Build.VERSION_CODES.KITKAT)
+		{
+			getWindow().getDecorView().setSystemUiVisibility(flagsK);
+			// Code below is to handle presses of Volume up or Volume down.
+			// Without this, after pressing volume buttons, the navigation bar will
+			// show up and won't hide
+			final View decorView = getWindow().getDecorView();
+			decorView
+					.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener()
+					{
+						@Override
+						public void onSystemUiVisibilityChange(int visibility)
+						{
+							if((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0)
+							{
+								decorView.setSystemUiVisibility(flagsK);
+							}
+						}
+					});
+		}
+		if(currentApiVersion >= Build.VERSION_CODES.JELLY_BEAN)
+		{
+			getWindow().getDecorView().setSystemUiVisibility(flagsJ);
+			// Code below is to handle presses of Volume up or Volume down.
+			// Without this, after pressing volume buttons, the navigation bar will
+			// show up and won't hide
+			final View decorView = getWindow().getDecorView();
+			decorView
+					.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener()
+					{
+						@Override
+						public void onSystemUiVisibilityChange(int visibility)
+						{
+							if((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0)
+							{
+								decorView.setSystemUiVisibility(flagsJ);
+							}
+						}
+					});
+		}
 
 	}
 }
